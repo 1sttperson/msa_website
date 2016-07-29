16f9f52ac94d6db4cf1ce229744dd437912c8b0d
Peter <professionalgalloway@gmail.com>
2016-07-29 22:06:02 +1200

    made memes from api splurge to main screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
-$(document).on('load', function () {
+$(document).ready(function () {
   $.ajax({
     url: "https://api.imgflip.com/get_memes",
     method: "GET",
     success: function (data) {
       var memes = data.data.memes;
       for (var i = 0; i < memes.length; i++) {
+        var meme = document.createElement("img");
+        meme.height = memes[i].height;
+        meme.width = memes[i].width;
+        meme.src = memes[i].url;
+        document.body.appendChild(meme);
         console.log(memes[i]);
       }
     }
   });
-
-
-
-
 });
