1431bdd49b4884ca198c89b514a3c68f6a8b32c4
Peter <professionalgalloway@gmail.com>
2016-09-15 00:12:07 +1200

    Your website has been memed =)

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
-$(document).ready(function () {
-  $.ajax({
-    url: "https://api.imgflip.com/get_memes",
-    method: "GET",
-    success: function (data) {
-      var memes = data.data.memes;
-      for (var i = 0; i < memes.length; i++) {
-        var meme = document.createElement("img");
-        meme.height = memes[i].height;
-        meme.width = memes[i].width;
-        meme.src = memes[i].url;
-        document.body.appendChild(meme);
-        console.log(memes[i]);
-      }
-    }
-  });
+$(function ($) {
+    $.ajax({
+        url: "billgen-API.php?default=1",
+        method: "POST",
+        data: "text=Stephen+is+a+silly+sausage+who+drinks+at+starbucks",
+        success: function (data) {
+            console.log(data.data);
+            console.log("hello");
+        }
+    });
+    $("#sendRequest").on("click", function () {
+        var name = $("#nameField").val();
+        var gender = $('input[name=gender]:checked').val();
+        $("#billImage").attr("src", "http://belikebill.azurewebsites.net/billgen-API.php?default=1&name=" + name + "&sex=" + gender);
+    });
 });
